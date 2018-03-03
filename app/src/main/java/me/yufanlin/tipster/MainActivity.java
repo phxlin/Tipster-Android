c456134d9344aba67ecb08b954465bf701021181
phxlin <yfln11@gmail.com>
2018-03-03 18:42:46 -0500

    Start project

---
+++ app/src/main/java/me/yufanlin/tipster/MainActivity.java
@@ -0,0 +1,13 @@
+package me.yufanlin.tipster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
