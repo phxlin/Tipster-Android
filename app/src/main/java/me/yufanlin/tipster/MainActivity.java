fad61f0db2587790df13ed1f5a4d80e276ce48a0
phxlin <yfln11@gmail.com>
2018-03-04 23:32:08 -0500

    Add tip percentage

--- app/src/main/java/me/yufanlin/tipster/MainActivity.java
+++ app/src/main/java/me/yufanlin/tipster/MainActivity.java
@@ -1,19 +1,68 @@
 package me.yufanlin.tipster;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tipAmountTextView) TextView tipTextView;
+    @BindView(R.id.totalAmountTextView) TextView totalTextView;
+    @BindView(R.id.tipSpinner) Spinner tipSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        SharedPreferences prefs = getSharedPreferences(PrefsActivity.MY_GLOBAL_PREFS, MODE_PRIVATE);
+
+        double mLow = (getDouble(prefs, PrefsActivity.LOW_KEY, 10))/100;
+        double mMid = (getDouble(prefs, PrefsActivity.MID_KEY, 20))/100;
+        double mHigh = (getDouble(prefs, PrefsActivity.HIGH_KEY, 30))/100;
+
+        List<Double> tipPercentage = new ArrayList<>();
+        tipPercentage.add(mLow);
+        tipPercentage.add(mMid);
+        tipPercentage.add(mHigh);
+
+        ArrayAdapter<Double> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, tipPercentage);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tipSpinner.setAdapter(adapter);
+
+        tipSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, adapterView.getItemAtPosition(i) + " is selected", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     @Override
@@ -32,5 +81,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Get double
+    double getDouble(final SharedPreferences prefs, final String key, final double defaultValue) {
+        return Double.longBitsToDouble(prefs.getLong(key, Double.doubleToLongBits(defaultValue)));
     }
 }
--- app/src/main/java/me/yufanlin/tipster/PrefsActivity.java
+++ app/src/main/java/me/yufanlin/tipster/PrefsActivity.java
@@ -1,22 +1,71 @@
 package me.yufanlin.tipster;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.security.acl.LastOwnerException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class PrefsActivity extends AppCompatActivity {
+
+    public static final String MY_GLOBAL_PREFS = "my_global_preferences";
+    public static final String LOW_KEY = "low_key";
+    public static final String MID_KEY = "mid_key";
+    public static final String HIGH_KEY = "high_key";
+    SharedPreferences.Editor editor;
+
+    @BindView(R.id.lowEditText) TextView mLowEditView;
+    @BindView(R.id.midEditText) TextView mMidEditView;
+    @BindView(R.id.highEditText) TextView mHighEditView;
+
+    @OnClick(R.id.lowEditText) void onEtvLowClick() {
+        editor = getSharedPrefs().edit();
+        putDouble(editor, LOW_KEY, Double.parseDouble(mLowEditView.getText().toString())).apply();
+    }
+    @OnClick(R.id.midEditText) void onEtvMidClick() {
+        editor = getSharedPrefs().edit();
+        putDouble(editor, MID_KEY, Double.parseDouble(mMidEditView.getText().toString())).apply();
+    }
+    @OnClick(R.id.highEditText) void onEtvHighClick() {
+        editor = getSharedPrefs().edit();
+        putDouble(editor, HIGH_KEY, Double.parseDouble(mHighEditView.getText().toString())).apply();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prefs);
+
+        ButterKnife.bind(this);
+
+        SharedPreferences prefs = getSharedPreferences(PrefsActivity.MY_GLOBAL_PREFS, MODE_PRIVATE);
+
+        mLowEditView.setText(String.valueOf((int)(getDouble(prefs, PrefsActivity.LOW_KEY, 0.10))));
+        mMidEditView.setText(String.valueOf((int)(getDouble(prefs, PrefsActivity.MID_KEY, 0.20))));
+        mHighEditView.setText(String.valueOf((int)(getDouble(prefs, PrefsActivity.HIGH_KEY, 0.30))));
     }
 
-    public static class SettingsFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-        }
+    //Put double
+    SharedPreferences.Editor putDouble(final SharedPreferences.Editor edit, final String key, final double value) {
+        return edit.putLong(key, Double.doubleToRawLongBits(value));
+    }
+
+    //Get double
+    double getDouble(final SharedPreferences prefs, final String key, final double defaultValue) {
+        return Double.longBitsToDouble(prefs.getLong(key, Double.doubleToLongBits(defaultValue)));
+    }
+
+    //Get shared preference
+    SharedPreferences getSharedPrefs() {
+        return getSharedPreferences(MY_GLOBAL_PREFS, MODE_PRIVATE);
     }
 }
